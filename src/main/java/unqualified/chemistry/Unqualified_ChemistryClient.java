3922bc59af8baa5c53dd540784fa45dcbf745341
VincentChen2 <chen.vincent.japan@gmail.com>
2025-10-27 00:23:33 -0400

    Fixed Beaker not being Transparent

--- src/main/java/unqualified/chemistry/Unqualified_ChemistryClient.java
+++ src/main/java/unqualified/chemistry/Unqualified_ChemistryClient.java
@@ -1,8 +1,11 @@
 package unqualified.chemistry;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
+import net.minecraft.client.render.BlockRenderLayer;
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
+import unqualified.chemistry.block.ModBlocks;
 import unqualified.chemistry.block.entity.ModBlockEntities;
 import unqualified.chemistry.block.entity.renderer.BeakerBlockEntityRenderer;
 import unqualified.chemistry.screen.ModScreenHandlers;
@@ -14,5 +17,9 @@ public class Unqualified_ChemistryClient implements ClientModInitializer {
     public void onInitializeClient() {
         BlockEntityRendererFactories.register(ModBlockEntities.BEAKER_BE, BeakerBlockEntityRenderer::new);
         HandledScreens.register(ModScreenHandlers.BEAKER_SCREEN_HANDLER, BeakerScreen::new);
+
+        BlockRenderLayerMap.putBlock(ModBlocks.BEAKER_BLOCK, BlockRenderLayer.CUTOUT);
+        BlockRenderLayerMap.putBlock(ModBlocks.BEAKER_BLOCK, BlockRenderLayer.TRANSLUCENT);
+
     }
 }
